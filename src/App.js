90da15b0a4aa5f7fc874b9cf13064fbede44e6ae
Swarna <swarna.prakashm@gmail.com>
2019-06-13 15:52:37 +0100

    Added the SearchComponent

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import AddExpenses from './components/AddExpenses';
+import SearchExpenses from './components/SearchExpenses';
 
 
 class App extends Component {
@@ -17,6 +18,10 @@ class App extends Component {
         <div><br></br></div>
         <div className="row">
           <AddExpenses />
+        </div>
+        <div><br></br></div>
+        <div className="row">
+          <SearchExpenses />
         </div>
       </div>
 
