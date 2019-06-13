dc413d2c701f4bc42a19c9e74387a641f00c802b
Swarna <swarna.prakashm@gmail.com>
2019-06-13 11:27:19 +0100

    Created source Code for frontend expenses manager application

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    render() {
+        return (
+            <div class="col-sm-12 title">
+                    <h1>Expenses Manager</h1>
+            </div>
+              
+        );
+    }
+}
+
+export default Header;
