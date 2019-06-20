f00c74a589fae5e801df11e37fe9c3e48506f471
pshital <skpawar@gmail.com>
2019-06-20 14:31:16 +0100

    Updates

---
+++ precache-manifest.6f3a94111ba7032e07a1116b08627f60.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a94aec2de42b260a7ffaf706213141b4",
+    "url": "/repository-name/index.html"
+  },
+  {
+    "revision": "2759936b995150c80893",
+    "url": "/repository-name/static/css/main.ae11baec.chunk.css"
+  },
+  {
+    "revision": "bc8c4fdce3af47743a7a",
+    "url": "/repository-name/static/js/2.cf4c32b4.chunk.js"
+  },
+  {
+    "revision": "2759936b995150c80893",
+    "url": "/repository-name/static/js/main.8b9128e3.chunk.js"
+  },
+  {
+    "revision": "6e2fc4b7d77a9c53df47",
+    "url": "/repository-name/static/js/runtime~main.f7634681.js"
+  }
+]);
