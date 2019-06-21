9774f7398a8b389439ef14075c738e4eceb42907
Swarna <swarna.prakashm@gmail.com>
2019-06-21 13:17:16 +0100

    Css Styles Changed

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ class App extends Component {
 
   };
 
-  apiEndpoint = "https://bsl6r2vqp7.execute-api.eu-west-2.amazonaws.com/dev/expenses";
+  apiEndpoint = "https://qlv8kacvn9.execute-api.eu-west-2.amazonaws.com/dev/expenses";
 
   componentDidMount = () => {
     axios.get(this.apiEndpoint)
@@ -142,7 +142,7 @@ class App extends Component {
           <SearchExpenses />
         </div>
         <div><br></br></div>
-        <div className="row expensesList">
+        <div className="row expensesList ">
           <div className="col bold">Expenses</div>
           <div className="col bold">Exp Date</div>
           <div className="col bold">Category</div>
@@ -150,9 +150,9 @@ class App extends Component {
           <div className="col bold">Payment</div>
           <div className="col bold">Notes</div>
           <div className="col bold">Amount</div>
-          <div className="coll bold"></div>
-          <div className="coll bold"></div>
-          <div className="container">
+          <div className="coll bold">Update</div>
+          <div className="coll bold">Delete</div>         
+          <div className="container ">
             {
               this.state.listofItems.map((item, index) => {
                 return <ExpensesList
