febd8a5163447d955f821c673bc72b88fedef3a5
Sarah Adlington <cheshire6655@icloud.com>
2019-06-22 17:02:33 +0100

    div's added to realign boxes, altered spelling

--- src/components/AddExpenses.js
+++ src/components/AddExpenses.js
@@ -83,86 +83,89 @@ class AddExpenses extends Component {
     }
     render() {
         return (
-
-            <div className="addExpeneses">
-                <div className="row divheight">
-                    <div class="col-md-4 col-sm-6">
-                        <div >
-                            <span class="input-group-text" id="basic-addon1">Expenses Name
+            <div class="container">
+                <div className="addExpeneses">
+                    <div className="row divheight">
+                        <div class="col-md-4 col-sm-6">
+                            <div >
+                                <span class="input-group-text" id="basic-addon1">Expenses Name
                             <input type="text" class="form-control" id="expenseName" onChange={this.onTextChanged} value={this.state.expenseName} /></span>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-md-4 col-sm-6">
-                        <div class="form-group">
-                            <span class="input-group-text" id="basic-addon1" for="ExpensesCategory">Category
+                        <div class="col-md-4 col-sm-6">
+                            <div class="form-group">
+                                <span class="input-group-text" id="basic-addon1" for="ExpensesCategory">Category
                                 <select class="form-control" id="ExpensesCategory" onChange={this.onCategoryChanged} value={this.state.category}>
-                                    <option>Select</option>
-                                    <option>Food</option>
-                                    <option>Travel</option>
-                                    <option>Education</option>
-                                    <option>Holiday</option>
-                                    <option>Shopping</option>
-                                    <option>Home</option>
-                                </select></span>
+                                        <option>Select</option>
+                                        <option>Food</option>
+                                        <option>Travel</option>
+                                        <option>Education</option>
+                                        <option>Holiday</option>
+                                        <option>Shopping</option>
+                                        <option>Home</option>
+                                    </select></span>
+                            </div>
                         </div>
-                    </div>
 
-                    <div class="col-md-4 col-sm-6">
-                        <div>
-                            <span class="input-group-text" id="basic-addon1">Amount
+                        <div class="col-md-4 col-sm-6">
+                            <div>
+                                <span class="input-group-text" id="basic-addon1">Amount
                             <input type="text" class="form-control" id="amount" onChange={this.onAmountChanged} placeholder="0" value={this.state.expenseAmount} /></span>
+                            </div>
                         </div>
+
                     </div>
 
-                </div>
-
-                <div className="row divheight">
-                    <div class="col-md-4 col-xs-6">
-                        <div >
-                            <span class="input-group-text" id="basic-addon1">Expeneses Date
+                    <div className="row divheight">
+                        <div class="col-md-4 col-xs-6">
+                            <div >
+                                <span class="input-group-text" id="basic-addon1">Expenses Date
                             <input id="expDate" class="form-control" type="date" onChange={this.onDateChange} value={this.state.expenseDate} /></span>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-md-4 col-xs-6">
-                        <div class="form-group">
-                            <span class="input-group-text" id="basic-addon1" for="PaymentType">Payment Type
+                        <div class="col-md-4 col-xs-6">
+                            <div class="form-group">
+                                <span class="input-group-text" id="basic-addon1" for="PaymentType">Payment Type
                             <select class="form-control" id="PaymentType" onChange={this.onPaymentChanged} value={this.state.paymentType}>
-                                    <option>Select</option>
-                                    <option>Card</option>
-                                    <option>Cash</option>
-                                    <option>Others</option>
-                                </select></span>
+                                        <option>Select</option>
+                                        <option>Card</option>
+                                        <option>Cash</option>
+                                        <option>Others</option>
+                                    </select></span>
+                            </div>
                         </div>
-                    </div>
 
 
-                    <div class="col-md-4 col-xs-6">
-                        <div class="form-group">
-                            <span class="input-group-text" id="basic-addon1" for="ExpensesStatus">Status
+                        <div class="col-md-4 col-xs-6">
+                            <div class="form-group">
+                                <span class="input-group-text" id="basic-addon1" for="ExpensesStatus">Status
                             <select class="form-control" id="ExpensesStatus" onChange={this.onStatusChanged} value={this.state.status}>
-                                    <option>Select</option>
-                                    <option>Paid</option>
-                                    <option>UnPaid</option>
-                                </select></span>
+                                        <option>Select</option>
+                                        <option>Paid</option>
+                                        <option>UnPaid</option>
+                                    </select></span>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="row divheight">
-                    <div class="col-md-7 col-sm-6">
-                        <span class="input-group-text" id="basic-addon1" for="ExpNotes">Notes
+                    <div class="container">
+                    <div className="row divheight">
+                        <div class="col-md-7 col-sm-6">
+                            <span class="input-group-text" id="basic-addon1" for="ExpNotes">Notes
                         <textarea class="form-control" id="ExpNotes" rows="1" onChange={this.onNotesChanged} value={this.state.notes}></textarea></span>
+                        </div>
+
+
+                        <div class="col-md-1 col-sm-6">
+                            <button type="button" class="btn btn-lg" onClick={this.onSaveClicked} >Save</button>
+                        </div>
+
+                        <div class="col-md-1 col-sm-6">
+                            <button type="button" class="btn btn-lg" onClick={this.onCancelClicked}>Cancel</button>
+                        </div>
+
                     </div>
-
-
-                    <div class="col-md-1 col-sm-6">
-                        <button type="button" class="btn btn-lg" onClick={this.onSaveClicked} >Save</button>
-                    </div>
-
-                    <div class="col-md-1 col-sm-6">
-                        <button type="button" class="btn btn-lg" onClick={this.onCancelClicked}>Cancel</button>
-                    </div>
-
                 </div>
+            </div>
             </div>
         );
     }
